017285c5623e4df63084922b12ec699f1b11ea7e
bmok02 <bmok.bmok@gmail.com>
2024-05-31 02:02:06 +0800

    some progress (not sure if it works tho)

--- loginpage/IDAndPasswords.java
+++ loginpage/IDAndPasswords.java
@@ -1,29 +1,14 @@
 package loginpage;
 
 public class IDAndPasswords {
+    private ImList<Pair<String, String>> loginPairList;
+
     
-    IDAndPasswords() {
-        try {
-            Connection connection = DriverManager.getConnection(
-                "jdbc:sql://localhost:3306/login_schema",
-                "root",
-                "password"
-            );
-    
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM USERS");
-            ImList<Pair<String, String>> loginPairList = new ImList<Pair<String, String>> ();
+    IDAndPasswords(ImList<Pair<String, String>> loginPairList) {
+        this.loginPairList = loginPairList;
+    }
 
-            while (resultSet.next()) {
-
-                Pair<String, String> loginPair = resultSet.getString("username", "password_hash")
-                loginPairList = loginPairList.add(loginPair);
-            }
-
-            System.out.println(loginPairList);
-            
-        } catch(SQLException e) {
-                e.printStackTrace();
-        }
+    protected ImList<Pair<String, String>> getLoginInfo() {
+        return this.loginPairList;
     }
 }
