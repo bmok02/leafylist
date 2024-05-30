76f206e4809a19e104b970d133714b9177c60a00
ji3jieee <165247147+ji3jieee@users.noreply.github.com>
2024-05-30 21:57:03 +0800

    Add files via upload

---
+++ wjcode/AppFrame.java
@@ -0,0 +1,69 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.border.Border;
+
+import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class AppFrame extends JFrame {
+    private TitleBar title;
+    private List list;
+    private ButtonPanel btnPanel;
+
+    private JButton addTask;
+    private JButton clear;
+
+    //constructor
+    AppFrame() {
+        this.setSize(400, 700);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+        title = new TitleBar();
+        list = new List();
+        btnPanel = new ButtonPanel();
+        this.add(title, BorderLayout.NORTH);
+        this.add(btnPanel, BorderLayout.SOUTH);
+
+        this.add(list, BorderLayout.CENTER);
+
+        addTask = btnPanel.getAddTask();
+        clear = btnPanel.getClear();
+
+        addListeners();
+    }
+
+    public void addListeners() {
+        addTask.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                Task task = new Task();
+                list.add(task);
+                list.updateNumbers();
+
+                task.getDone().addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mousePressed(MouseEvent e) {
+                        task.changeState();
+                        revalidate();
+                    }
+                });
+                revalidate();
+            }
+        });
+
+        clear.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                list.removeCompletedTasks();
+                repaint();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        AppFrame frame = new AppFrame();
+    }
+
+}
